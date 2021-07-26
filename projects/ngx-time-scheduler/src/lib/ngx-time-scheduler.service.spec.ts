d850c7a27d5892c51d552936978a85de8cba4afe
Semanova <Valeria.Semanova@t-systems.com>
2021-07-26 14:53:44 +0200

    Update package 1.1.8

--- projects/ngx-time-scheduler/src/lib/ngx-time-scheduler.service.spec.ts
+++ projects/ngx-time-scheduler/src/lib/ngx-time-scheduler.service.spec.ts
@@ -6,7 +6,7 @@ describe('NgxTimeSchedulerService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: NgxTimeSchedulerService = TestBed.get(NgxTimeSchedulerService);
+    const service: NgxTimeSchedulerService = TestBed.inject(NgxTimeSchedulerService);
     expect(service).toBeTruthy();
   });
 });
